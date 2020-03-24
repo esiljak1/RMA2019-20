bc6017f377414d97d86a2509dfe4c0a863009476
Emin Šiljak <esiljak1@etf.unsa.ba>
2020-03-24 20:32:23 +0100

    Kreiran novi activity

---
+++ app/src/main/java/com/example/rma20siljakemin84/EditTransactionActivity.java
@@ -0,0 +1,14 @@
+package com.example.rma20siljakemin84;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditTransactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_transaction);
+    }
+}
