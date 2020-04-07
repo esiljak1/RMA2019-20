9b167a53ee5492719358fcdcb0ae68cea8a24b2b
Emin Šiljak <esiljak1@etf.unsa.ba>
2020-04-07 15:30:44 +0200

    Napravljen novi fragment za gledanje account detalja

---
+++ app/src/main/java/com/example/rma20siljakemin84/AccountDetailsFragment.java
@@ -0,0 +1,17 @@
+package com.example.rma20siljakemin84;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class AccountDetailsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_account_details, container, false);
+    }
+}
