455f5455ba9abbd99896bf6481462159d82ab0a4
Emin Šiljak <esiljak1@etf.unsa.ba>
2020-04-08 13:46:23 +0200

    Dodan fragment za grafove

---
+++ app/src/main/java/com/example/rma20siljakemin84/GraphsFragment.java
@@ -0,0 +1,19 @@
+package com.example.rma20siljakemin84;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class GraphsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_graphs, container, false);
+    }
+}
